6ca5f92bf767320b73fedef3de2ea22855125cfb
Sebastian Bürgel <s.buergel@gmail.com>
2021-08-24 12:56:58 +0200

    added plots

--- ct.py
+++ ct.py
@@ -5,7 +5,7 @@ import matplotlib.pyplot as plt
 import hoprsim
 
 
-'''
+
 stake = [
    [0, 2, 0, 0, 0, 0, 8, 0, 12, 0], 
    [0, 0, 1, 0, 0, 0, 0, 7, 0, 0], 
@@ -25,17 +25,17 @@ stake = [
    [0, 0, 10], 
    [0, 5, 0]
 ]
-
-
+'''
+stake = hoprsim.setupStake(200, 3, 20, 10, 100000, 0.1)
 importance = hoprsim.calcImportance(stake)
-print("importance ", importance)
+# print("importance ", importance)
 sortedPrioList = [i[0] for i in sorted(enumerate(importance), key=lambda x:x[1], reverse=True)]
 print("sortedPrioList", sortedPrioList)
 
 
 
 ctChannel = [0] * len(stake)
-balancePerCtChannel = 500
+balancePerCtChannel = 5
 hops = 3
 n = len(stake)
 
@@ -44,7 +44,7 @@ n = len(stake)
 numTests = 1000
 totalPayout = [0] * len(stake)
 totalCtNodes = [0] * numTests
-ctNodeBalance = 1000
+ctNodeBalance = 50
 payoutPerHop = 1
 
 
@@ -83,15 +83,42 @@ for w in range(numTests):
    #print("nodes Payout", nodePayout) 
    #print("path indices:", pathIndices)
 #totalPayout = sum(nodePayout[w] for w in range (numTests))
+
+print("stake:")
+hoprsim.printArray2d(stake)
+
 print("payout", totalPayout)
+
 totalStake = numpy.sum(stake, axis=1)
-print("total stake", totalStake)
+
+print("total stake:")
+hoprsim.printArray1d(totalStake)
+
 print("importance", importance)
 # exp node 2 has been chosen 20 times for example  
 #table = [['total CT Nodes', 'total Payout'], [totalpathIndices, totalPayout]]
 #print("table", table)
 #accuracyAverage = [0.51,0.52,0.64,0.57,0.7,0.63,0.59,0.66,0.64]
 
+hoprsim.drawGraph(stake)
+plt.figure(2)
 
+minMarkerSize = 2
+maxMarkerSize = 20
+normalizedImportance = [0] * len(importance)
+minImportance = min(importance)
+maxImportance = max(importance)
+for x in range(len(importance)):
+    normalizedImportance[x] = float((importance[x] - minImportance) / (maxImportance - minImportance) * (maxMarkerSize - minMarkerSize) + minMarkerSize)
+print("normalizedImportance", normalizedImportance)
 
+for x, y, size, text in zip(totalStake, totalPayout, normalizedImportance, range(len(stake))):
+    plt.plot(x, y, ms=size, marker='o', color='r', linestyle='None')
+    plt.text(x, y, text)
+plt.title('Stake vs payout')
+plt.xlabel('Stake')
+plt.ylabel('Payout')
+plt.yscale('log')
+plt.xscale('log')
+plt.show()
 
--- hoprsim.py
+++ hoprsim.py
@@ -100,6 +100,9 @@ def randomPickWeightedByImportance(importance):
     channel = selectChannel(importance, [i for i in range(len(importance))])
     
     return channel
+
+
+
 # opens a random payment channel given its importance
 def openInitialCtChannels(ctNodeBalance, balancePerCtChannel, importance):
     ctChannelBalances = [0 for i in range(len(importance))]
@@ -107,3 +110,124 @@ def openInitialCtChannels(ctNodeBalance, balancePerCtChannel, importance):
         ctChannelBalances[randomPickWeightedByImportance(importance)] += balancePerCtChannel
         ctNodeBalance -= balancePerCtChannel
     return ctChannelBalances, ctNodeBalance
+
+
+
+def drawGraph(stake):
+    """
+    draws a network graph based on a square stake matrix with 0-diagonale
+    the stake matrix has channel opener in rows and counterparty in columns
+    each entry in the stake matrix represents the value staked in channel (0 for no channel)
+
+    Parameters
+    ----------
+    stake: stake matrix
+    """
+    # parameters for drawing
+    minLineWeight = 1
+    maxLineWeight = 4
+    blue1 = '#0000b4'
+    blue2 = '#b4f0ff'
+    blue3 = '#000050'
+    blue4 = '#3c64a5'
+    yellow = '#ffffa0'
+    edgeColorA = blue1
+    edgeColorB = blue4
+    nodeColor = yellow
+    nodeSize = 500
+    labelFont = 'Source Code Pro'
+    connectionstyle = 'arc3, rad=0.0'
+    arrowstyle = '->'
+    # /parameters
+
+    G=nx.OrderedDiGraph()
+    stake1d = numpy.concatenate(stake)
+    maxS = max(stake1d)
+    minS = min(stake1d[numpy.nonzero(stake1d)])
+    bottomEdges = []
+    topEdges = []
+    bottomWeights = []
+    topWeights = []
+    bottomColors = []
+    topColors = []
+    print("maxS: ", maxS)
+    print("minS: ", minS)
+
+    for x in range(len(stake)):
+        for y in range(x):
+            weightA = 0
+            weightB = 0
+            if(stake[x][y] > 0):
+                weightA = (stake[x][y] - minS) / (maxS - minS) * (maxLineWeight - minLineWeight) + minLineWeight
+                G.add_edge(x, y, weight=weightA, color=edgeColorA)
+            if(stake[y][x] > 0):
+                weightB = (stake[y][x] - minS) / (maxS - minS) * (maxLineWeight - minLineWeight) + minLineWeight
+                G.add_edge(y, x, weight=weightB, color=edgeColorB)
+
+        # plot in the right order to have the fatter edge on the bottom
+            if(weightA > 0 and weightB > 0):
+                if(weightA > weightB):
+                    bottomEdges.append((x,y))
+                    topEdges.append((y,x))
+                    bottomWeights.append(weightA)
+                    topWeights.append(weightB)
+                    bottomColors.append(edgeColorA)
+                    topColors.append(edgeColorB)
+                else:
+                    bottomEdges.append((y,x))
+                    topEdges.append((x,y))
+                    bottomWeights.append(weightB)
+                    topWeights.append(weightA)
+                    bottomColors.append(edgeColorB)
+                    topColors.append(edgeColorA)
+            elif(weightA > 0):
+                topEdges.append((x,y))
+                topWeights.append(weightA)
+                topColors.append(edgeColorA)
+            elif(weightB > 0):
+                topEdges.append((y,x))
+                topWeights.append(weightB)
+                topColors.append(edgeColorB)
+
+    weights = list(nx.get_edge_attributes(G,'weight').values())
+    colors = list(nx.get_edge_attributes(G,'color').values())
+    pos = nx.circular_layout(G)
+
+    nx.draw_networkx_nodes(G, pos, node_size=nodeSize, node_color=nodeColor)
+    nx.draw_networkx_labels(G, pos, font_family=labelFont)
+    nx.draw_networkx_edges(
+        G,
+        pos=pos,
+        width=bottomWeights,
+        edge_color=bottomColors,
+        edgelist=bottomEdges,
+        arrows=True,
+        connectionstyle=connectionstyle,
+        arrowstyle=arrowstyle
+    )
+    nx.draw_networkx_edges(
+        G,
+        pos=pos,
+        width=topWeights,
+        edge_color=topColors,
+        edgelist=topEdges,
+        arrows=True,
+        connectionstyle=connectionstyle,
+        arrowstyle=arrowstyle
+    )
+
+#    plt.show()
+
+
+def printArray2d(a):
+    for row in range(len(a)):
+        for col in range (len(a[row])):
+            print("{:4.1f}".format(a[row][col]), end = " ")
+        print()
+
+
+
+def printArray1d(a):
+    for row in range(len(a)):
+        print("{:4.1f}".format(a[row]), end = " ")
+
