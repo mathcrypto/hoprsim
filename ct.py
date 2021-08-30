8a4621e01da1648a8e4100937425b38879f64716
mathcrypto <bouguera_amira@yahoo.fr>
2021-08-30 10:38:45 +0200

    max value

--- ct.py
+++ ct.py
@@ -7,21 +7,22 @@ import hoprsim
 
 
 stake = [
-   [0, 2, 0, 0, 0, 0, 8, 0, 12, 0], 
-   [0, 0, 1, 0, 0, 0, 0, 7, 0, 0], 
-   [0, 11, 0, 1, 0, 0, 8, 0, 0, 0], 
-   [0, 2, 0, 0, 0, 0, 8, 0, 0, 0],
-   [0, 0, 2, 0, 0, 0, 0, 13, 0, 0],
-   [0, 0, 0, 14, 9, 0, 0, 5, 0, 0], 
-   [0, 0, 0, 23, 6, 0, 0, 0, 9, 0],
-   [0, 2, 0, 11, 0, 0, 3, 0, 0, 20], 
-   [0, 0, 0, 0, 10, 0, 5, 0, 0, 0], 
-   [0, 3, 5, 0, 0, 7, 0, 1, 9, 0] 
+   [0, 2, 0, 0, 0, 0, 8, 0, 12, 0,1], 
+   [0, 0, 1, 0, 0, 0, 0, 7, 0, 0,2], 
+   [0, 11, 0, 1, 0, 0, 8, 0, 0, 0,0], 
+   [0, 2, 0, 0, 0, 0, 8, 0, 0, 0,0],
+   [0, 0, 2, 0, 0, 0, 0, 13, 0, 0,0],
+   [0, 0, 0, 14, 9, 0, 0, 5, 0, 0,4], 
+   [0, 0, 0, 23, 6, 0, 0, 0, 9, 0,0],
+   [0, 2, 0, 11, 0, 0, 3, 0, 0, 20,13], 
+   [0, 0, 0, 0, 10, 0, 5, 0, 0, 0,3], 
+   [0, 3, 5, 0, 0, 7, 0, 1, 9, 0,0] ,
+   [0, 3, 21, 3, 11, 7, 7, 1, 9, 8,0] 
 ]
 
+
+
 '''
-
-
 stake = [
    [0, 20, 0, 2], 
    [0, 0, 5, 1], 
@@ -35,9 +36,12 @@ importance = hoprsim.calcImportance(stake)
 sortedPrioList = [i[0] for i in sorted(enumerate(importance), key=lambda x:x[1], reverse=True)]
 print("sortedPrioList", sortedPrioList)
 
+   
+   
+
 ctChannel = [0] * len(stake)
 balancePerCtChannel = 5
-hops = 5
+hops = 3
 n = len(stake)
 
 #ctChannelBalances, ctNodeBalance = hoprsim.openInitialCtChannels(ctNodeBalance, balancePerCtChannel, importance)
@@ -64,7 +68,8 @@ for w in range(numTests):
    nodePayout = [0] * n
    for j in range (hops):
       pathIndices[j] = hoprsim.randomPickWeightedByImportance(importanceTmp)
-      #print("importance", importance)
+      print("importance", importance)
+      print("path", pathIndices[j])
       # reset importance
       importanceTmp = list(importance)
       #print("importance", importanceTmp)
@@ -122,23 +127,36 @@ for i in range(len(stake)):
       for p in q:
          sumImportance += importance[p]
 
-      #sumImportance= numpy.sum(importance[p]) 
       print("sum Importance", sumImportance)
       # Now we add those sum of importances of other nodes to each of the path nodes to a list and then we compare them
       # the node from the path that has the largest importance of other nodes with whom it has open channels with is the most strategic one 
       listImp.append(sumImportance)
 
 print("list", listImp)
+
 # find the maximum importance value in this list
-max_value = max(listImp)
-max_index = listImp.index(max_value)
-print("maxIndex", max_index)
-print("Best node", path[max_index])
+indices = [i for i, x in enumerate(listImp) if x == max(listImp)]
+
+
+for i in indices:
+   for j in indices:
+      if (importance[path[i]] > importance[path[j]]):
+         maxIndex = i 
+print("Best node", path[maxIndex])
 
 
 
       
       
+
+# Strategy: consider node's importance as a parameter, the open channels it has with other nodes and their importance
+#for i in range (len(importance)):
+  #if importance[i] !=  0 :
+       
+
+
+
+
 
 # node with highest payout or a pretty high payout and smaller stake is a positive outlier
 l = []
@@ -158,6 +176,14 @@ print("Node with highest payout", bestNode)
 
 
 
+# now that we know who is the best node (meaning the one with largest payout) we can find out why did it receive more payout than others?
+# Is is because it has the largest importance? --> Not necessarly. We have seen in some test cases that outlier nodes with less importance can receive largest Payout
+# Then it must be because it has more open channels with important nodes than others
+ 
+
+
+
+
 #print("stake:")
 #hoprsim.printArray2d(stake)
 stakeCopy = stake[:]
@@ -166,11 +192,17 @@ stakeCopy = stake[:]
 print("payout", totalPayout)
 
 totalStake = numpy.sum(stake, axis=1)
-
 #print("total stake:")
-hoprsim.printArray1d(totalStake)
+#hoprsim.printArray1d(totalStake)
 
 print("importance", importance)
+# exp node 2 has been chosen 20 times for example  
+#table = [['Node's importance', 'total Payout']]
+#print("table", table)
+#accuracyAverage = [0.51,0.52,0.64,0.57,0.7,0.63,0.59,0.66,0.64]
+
+
+
 
 
 
@@ -185,6 +217,7 @@ maxImportance = max(importance)
 for x in range(len(importance)):
     normalizedImportance[x] = float((importance[x] - minImportance) / (maxImportance - minImportance) * (maxMarkerSize - minMarkerSize) + minMarkerSize)
 print("normalizedImportance", normalizedImportance)
+
 
 for x, y, size, text in zip(totalStake, totalPayout, normalizedImportance, range(len(stake))):
     plt.plot(x, y, ms=size, marker='o', color='r', linestyle='None')
